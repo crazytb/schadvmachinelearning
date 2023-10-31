8eea5c82c114a981b65d067b76fa488277c4a7da
Taewon Song <crazytb@gmail.com>
2023-11-01 00:38:35 +0900

    Add files via upload

--- mymodule.py
+++ mymodule.py
@@ -1,8 +1,15 @@
 def world():
     print("Hello, World!")
     
+def _world(word):
+    print("_Hello, World!")
+    
 def add(a, b):
     return a + b
 
 def sub(a, b):
     return a - b
+
+print("Outside mymodule.py")
+if __init__ == "__main__":
+    print("Inside mymodule.py")
